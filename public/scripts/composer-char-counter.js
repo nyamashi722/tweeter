a62d8fa41f435e2a8906c6f62feff7416cb00f6a
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-10-12 20:35:57 -0400

    clean up code through linting

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -8,6 +8,7 @@ $(document).ready(function () {
     const charactersLeft = counter - $userInput.length
     $(".counter").text(charactersLeft)
     if (charactersLeft < 0) {
+      // add a class of .over-limit, which in the css file, will make the text color red
       $(".counter").addClass("over-limit")
     } else {
       $(".counter").removeClass("over-limit")
